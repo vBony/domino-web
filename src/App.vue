c09dd4dc747e0974c9c304e647c7f6cd3045d193
Vinicius de Assis Ferreira <vini_07-ferreira@hotmail.com>
2024-08-15 22:45:30 -0300

    arquitetura com login

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="w100">
+		<nav class="navbar navbar-expand-lg bg-body-tertiary">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="#">dmno</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+						<li class="nav-item">
+							<RouterLink to="/" class="nav-link active" aria-current="page">Home</RouterLink>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Features</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Pricing</a>
+						</li>
+					</ul>
+
+					<div class="d-flex">
+						<RouterLink to="/login" class="btn btn-success">Jogar</RouterLink>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</div>
+
+	<main class="container py-4">
+		<RouterView />
+	</main>
+
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ src/helpers/http.ts
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import $ from "jquery";
+
+const req = () => {
+    const defaultOptions = {
+        baseURL: import.meta.env.VITE_SERVER_URL,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+  
+    // Create instance
+    let instance = axios.create(defaultOptions);
+  
+    // Set the AUTH token for any request
+    instance.interceptors.request.use(function (config) {
+        $('#loading').show()
+        const token = localStorage.getItem("dmno");
+        config.headers.Authorization =  token ? `Bearer ${token}` : '';
+        return config;
+    }, 
+    (error) => {
+        $('#loading').hide()
+        return Promise.reject(error);
+    });
+
+    instance.interceptors.response.use((response) => {
+        $('#loading').hide()
+        return response;
+    }, (error) => {
+        $('#loading').hide()
+        return Promise.reject(error);
+    });
+  
+    return instance;
+};
+  
+export default req();
