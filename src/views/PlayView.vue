b8409299365df8aae0d7ebcfa0b542c4607f049f
Vinicius de Assis Ferreira <vini_07-ferreira@hotmail.com>
2024-08-18 21:58:03 -0300

    criando tela para jogar

---
+++ src/views/PlayView.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="row">
+    <h1>Jogar</h1>
+    <div class="row justify-content-center">
+        <div class="col-12 d-flex justify-content-center">
+            <div class="d-flex flex-column align-items-center justify-content-center mb-4">
+                <div class="text-center">
+                    <img src="https://picsum.photos/200/300?random=1" style="border-radius: 100%;width:55px;height: 55px;">
+                </div>
+                <span>Jogador 4</span>
+            </div>
+        </div>
+        <div class="row m-0">
+            <div class="col-1 col-lg-1 col-md-1 col-sm-1 col-xs-1 d-flex align-items-center">
+                <div class="d-flex flex-column align-items-center justify-content-center mb-4">
+                    <div class="text-center">
+                        <img src="https://picsum.photos/200/300?random=2" style="border-radius: 100%;width:55px;height: 55px;">
+                    </div>
+                    <span>Jogador 4</span>
+                </div>
+            </div>
+            <div class="col-10 col-lg-10 col-md-10 col-sm-10 col-xs-10">
+                <div class="card p-3 bg-dark" style="height: 60vh;border-color: var(--color-border) !important">
+                </div>
+            </div>
+            <div class="col-1 col-lg-1 col-md-1 col-sm-1 col-xs-1 d-flex align-items-center">
+                <div class="d-flex flex-column align-items-center justify-content-center mb-4">
+                    <div class="text-center">
+                        <img src="https://picsum.photos/200/300?random=3" style="border-radius: 100%;width:55px;height: 55px;">
+                    </div>
+                    <span>Jogador 4</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 d-flex justify-content-center mt-4">
+            <div class="d-flex flex-column align-items-center justify-content-center">
+                <div class="text-center">
+                    <img src="https://picsum.photos/200/300?random=4" style="border-radius: 100%;width:55px;height: 55px;">
+                </div>
+                <span>Jogador 1  (você)</span>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
