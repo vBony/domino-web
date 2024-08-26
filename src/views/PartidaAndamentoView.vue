dae70ea6b4fb443c30b638a03b268302e556561b
Vinicius de Assis Ferreira <vini_07-ferreira@hotmail.com>
2024-08-25 23:42:26 -0300

    tela para informar que já existe uma partida em andamento

---
+++ src/views/PartidaAndamentoView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <h1 class="w100">Partida em andamento 😖</h1>
+        <p>
+            Já existe uma partida em andamento, aguarde a finalização e tente novamente
+        </p>
+    
+        <div class="row mt-4">
+            <RouterLink to="/play" class="btn btn-success">Tentar Novamente</RouterLink>
+        </div>
+    </div>
+</template>
+    
+<script setup lang="ts">
+
+</script>
