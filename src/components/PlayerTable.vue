a58c7628d36700f3def94c245b455ca5400f90ae
Vinicius de Assis Ferreira <vini_07-ferreira@hotmail.com>
2024-08-26 22:15:23 -0300

    componente player

---
+++ src/components/PlayerTable.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="d-flex flex-column align-items-center justify-content-center mb-4">
+        <div class="text-center">
+            <img :src="'https://picsum.photos/200/300?random='+id" style="border-radius: 100%;width:55px;height: 55px;">
+        </div>
+        <span>{{ nickname }}</span>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+const SERVER_URL = import.meta.env.VITE_SERVER_URL
+
+const App = defineComponent({
+    props: {
+        id: null,
+        nickname: null
+    }
+})
+
+export default App
+</script>
